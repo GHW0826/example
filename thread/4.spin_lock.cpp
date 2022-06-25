a2c3886f4129c14d8996cff6c5dba48a536d6e40
KSS <96938324+GHW0826@users.noreply.github.com>
2022-06-25 20:50:31 +0900

    Create 4.spin_lock.cpp

---
+++ thread/4.spin_lock.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <atomic>
+#include <mutex>
+#include <unordered_map>
+using namespace std;
+
+// volatile : 컴파일러 최적화 하지말라.
+class SpinLock
+{
+public:
+    void lock() {
+        // cas (compare nad swap)
+        /*
+            bool expected;
+            bool desired;
+            if (locked_ == expected) {
+                expected = locked_;
+                locked_ = desired;
+                return true;
+            }
+            else {
+                expected = locked_;
+                return false;
+            }
+        */
+        bool expected = false;
+        bool desired = true;
+        while (locked_.compare_exchange_strong(expected, desired) == false)
+            expected = false;
+
+        locked_.store(true);
+    }
+
+    void unlock() {
+        locked_.store(false);
+    }
+private:
+    atomic<bool> locked_ = false;
+};
+
+int sum = 0;
+mutex m;
+SpinLock spinLock;
+
+void Add() {
+    for (int i = 0; i < 1'000'000; ++i) {
+        lock_guard<SpinLock> guard(spinLock);
+        ++sum;
+    }
+}
+
+void Sub() {
+    for (int i = 0; i < 1'000'000; ++i) {
+        lock_guard<SpinLock> guard(spinLock);
+        --sum;
+    }
+}
+int main()
+{
+    thread t1(Add);
+    thread t2(Sub);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
